ff549e7219ad24273e46164a5ee0af8ecb35d083
huangweiswat <44860141+huangweiswat@users.noreply.github.com>
2020-12-16 11:07:12 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJf1feKADDAM8OaxlZc3aOdL759KhGRbazf9fczg86PxOKcz-lDZrsdlUSkJbMdg2fySpXKRlI; pt_pin=329027330;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
